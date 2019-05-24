560c674c7acd81cee3dffc02e61f83c2256032a9
JennMil <50970213+JennMil@users.noreply.github.com>
2019-05-23 23:11:18 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+package org.my;
+
+public class main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Universidad UTE");
+		System.out.println("Hola Mundo");
+    }
+}
